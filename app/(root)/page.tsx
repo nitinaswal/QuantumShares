8f9f6e32866d11b2837013fefcef278b6030c297
nitinaswal <nitinaswal2002@gmail.com>
2025-10-18 11:13:03 +0530

    added Heatmap widget config

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,5 +1,5 @@
 import TradingViewWidget from "@/components/TradingViewWidget";
-import { MARKET_DATA_WIDGET_CONFIG, MARKET_OVERVIEW_WIDGET_CONFIG, TOP_STORIES_WIDGET_CONFIG } from "@/lib/constants";
+import { HEATMAP_WIDGET_CONFIG, MARKET_DATA_WIDGET_CONFIG, MARKET_OVERVIEW_WIDGET_CONFIG, TOP_STORIES_WIDGET_CONFIG } from "@/lib/constants";
 export default function Home() {
   const scriptUrl=`https://s3.tradingview.com/external-embedding/embed-widget-`;
   return (
@@ -19,7 +19,7 @@ export default function Home() {
             <TradingViewWidget  
               title="Stock Heatmap"
               scriptUrl={`${scriptUrl}stock-heatmap.js`}
-              config={MARKET_OVERVIEW_WIDGET_CONFIG} 
+              config={HEATMAP_WIDGET_CONFIG} 
               height={600}
             />
           </div>
