1dcd707ea9bc2078f86c502f1aea8244f174e226
nitinaswal <nitinaswal2002@gmail.com>
2025-10-29 02:13:50 +0530

    Worked on Logout functionality and Sign-in

--- components/UserDropdown.tsx
+++ components/UserDropdown.tsx
@@ -12,14 +12,15 @@ import { Button } from "./ui/button";
 import { Avatar, AvatarFallback, AvatarImage } from "./ui/avatar";
 import { LogOut } from "lucide-react";
 import NavItems from "./NavItems";
-const UserDropdown = () => {
+import { signOut } from "@/lib/actions/auth.actions";        
+
+const UserDropdown = ({ user }:{user:User}) => {
     const router=useRouter();
 
     const handleSignOut=async()=>{
+        await signOut();
         router.push("/sign-in");
     }
-
-    const user={name:"Nitin",email:"nitinaswal2002@gmail.com"};
     return (
         <DropdownMenu>
             <DropdownMenuTrigger asChild>
--- lib/actions/auth.actions.ts
+++ lib/actions/auth.actions.ts
@@ -2,6 +2,7 @@
 
 import {auth} from "@/lib/better-auth/auth";
 import {inngest} from "@/lib/inngest/client";
+import {headers} from "next/headers";
 
 export const signUpWithEmail = async(data:SignUpFormData)=>{
     try{
@@ -27,5 +28,25 @@ export const signUpWithEmail = async(data:SignUpFormData)=>{
     }catch(e){
         console.log("Sign up failed",e);
         return {success:false,error:'Sign up failed'}
+    }
+}
+
+export const signOut = async () => {
+    try {
+        await auth.api.signOut({ headers: await headers() });
+    } catch (e) {
+        console.log('Sign out failed', e)
+        return { success: false, error: 'Sign out failed' }
+    }
+}
+
+export const signInWithEmail = async ({ email, password }: SignInFormData) => {
+    try {
+        const response = await auth.api.signInEmail({ body: { email, password } })
+
+        return { success: true, data: response }
+    } catch (e) {
+        console.log('Sign in failed', e)
+        return { success: false, error: 'Sign in failed' }
     }
 }
